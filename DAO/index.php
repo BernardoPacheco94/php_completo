b3904a668810b724ea39bd41ae4fd897422bab97
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-08-13 17:37:07 -0300

    update

--- DAO/index.php
+++ DAO/index.php
@@ -1,31 +1,34 @@
 <?php
 
 require_once "config.php";
-
-//$user = new Usuario;
-
-//  $user->findById(4);
-
-//  echo $user;//apneas o echo, pois há o construtor toString na classe Usuario
+/*--CARREGA USUARIOS E MOSTRA NA TELA--*/
+$user = new Usuario;
+//$user->findById(4);
+//echo $user;//apneas o echo, pois há o construtor toString na classe Usuario
 
 
-
+/*--LISTA OS USUARIOS-- */
 // $list = Usuario::getList();
 // echo json_encode($list);
 
-
+/*-LISTA USUARIOS POR UM PEDAÇO DE PALAVRA-CHAVE-- */
 // $search = Usuario::search("be");
-
 // echo json_encode($search);
 
-
+/*--VALIDA USUÁRIO USUÁRIO--*/
 // $user->login("gabriel","papai123");
 
+
+/*--2 MANEIRAS DE FAZER INSERT-- */
 //$user = Usuario::insert("aabrao","12345");//insert sem procedure
 //$list = Usuario::getList();
+// $user = new Usuario("jubileu", "bora");
+// $user->insert();
+// echo $user;
 
-$user = new Usuario("jubileu", "bora");
-$user->insert();
+/*--UPDATE DE USUARIO--*/
+$user->findById(6);//carrega o usuário pelo id
+$user->update("mufasa","scar");
 
-echo $user;
-
+$lista = Usuario::getList();
+echo json_encode($lista);
