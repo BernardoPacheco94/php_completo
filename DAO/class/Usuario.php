a7c37b914fae548c4bfc455cef3b3715769d03ad
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-08-13 17:22:02 -0300

    insert

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -37,6 +37,12 @@ public function setDesSenha($value)
     $this->dessenha=$value;
 }
 
+public function __construct($login = "", $senha="")
+{
+    $this->setDesLogin($login);
+    $this->setDesSenha($senha);
+}
+
 
 public function findById ($id)
 {
@@ -46,14 +52,77 @@ public function findById ($id)
 
     if (isset($results[0]))//verifica se o array existe (posição 0)
     {
-        $row = $results[0];//joga o array na varável $row
-
-
-        $this->setIdUsuario($row['idusuario']);//seta o id com o indice idusuario que veio pelo array do select
-        $this->setDesLogin($row['deslogin']);
-        $this->setDesSenha($row['dessenha']);
-          
+        $this->setData($results[0]);          
     }
+
+}
+
+public function setData($data)
+{
+    $this->setIdUsuario($data['idusuario']);//seta o id com o indice idusuario que veio pelo array do select
+        $this->setDesLogin($data['deslogin']);
+        $this->setDesSenha($data['dessenha']);
+}
+
+public static function getList()//método estático, pois não há this. Pode ser instanciado junto com o objeto
+{
+    $sql = new sql;
+
+    return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+}
+
+public static function search($login)//localiza users por uma parte do login
+{
+    $sql = new sql;
+
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",array(":SEARCH"=>"%".$login."%"));
+    
+}
+
+public function login($login, $pass)//valida o acesso
+{
+    $sql = new sql();
+
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASS",array(":LOGIN"=>$login, ":PASS"=>$pass));//retorna um array e salva o array na results
+
+    if (isset($results[0]))//verifica se o array existe (posição 0)
+    {
+        $this->setData($results[0]);
+          
+    } else{
+        throw new Exception("Login e/ou senha inválidos");
+    }
+}
+
+public function insert()
+{
+    $sql = new sql;
+
+    //insert com procedure (criar a procedure no banco), que retorna do banco o id do usuario inserido.
+    $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASS)", array(":LOGIN"=>$this->getDesLogin(), ":PASS"=>$this->getDesSenha()));
+
+    if(isset($results[0]))
+    { 
+        $this->setData($results[0]);
+    }
+
+
+    
+    // $sql->execQuery("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :SENHA)", array(":LOGIN"=>$login, ":SENHA"=>$senha));//Sem procedure, podendo ser static
+}
+
+public function update($login, $senha)
+{
+    $this->setDesLogin($login);
+    $this->setDesSenha($senha);
+
+    $sql = new sql;
+
+    $sql->execQuery("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :SENHA WHERE idusuario = :ID", array(
+        ':LOGIN'=>$this->getDesLogin(),
+        ':SENHA'=>$this->getDesSenha(),
+        ':ID'=>$this->getIdUsuario(),
+    ));
 
 }
 
--- DAO/index.php
+++ DAO/index.php
@@ -2,8 +2,30 @@
 
 require_once "config.php";
 
-$user = new Usuario;
+//$user = new Usuario;
 
- $user->findById(2);
+//  $user->findById(4);
 
- echo $user;//apneas o echo, pois há o construtor toString na classe Usuario
+//  echo $user;//apneas o echo, pois há o construtor toString na classe Usuario
+
+
+
+// $list = Usuario::getList();
+// echo json_encode($list);
+
+
+// $search = Usuario::search("be");
+
+// echo json_encode($search);
+
+
+// $user->login("gabriel","papai123");
+
+//$user = Usuario::insert("aabrao","12345");//insert sem procedure
+//$list = Usuario::getList();
+
+$user = new Usuario("jubileu", "bora");
+$user->insert();
+
+echo $user;
+
