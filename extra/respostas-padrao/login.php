85563feeecf5307b5248e3964d09440cb8a6d099
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-08-22 17:46:56 -0300

    ajustes delete, visuais

--- extra/respostas-padrao/login.php
+++ extra/respostas-padrao/login.php
@@ -6,17 +6,9 @@ require_once "config.php";
 
 
 
-
-
 if (isset($_POST['btn_entrar'])){
     $name = $_POST['input_name'];
     $pass = $_POST['input_pass'];
- //$securePass = password_hash($pass, PASSWORD_DEFAULT);
-//$name = 'admin';
-//$pass = '$2y$10$.c/Hf9qyjicNKz82PZniM.j.77XdyxqWnJMMRbU45wjH2pOC43Cia';
-
-
-//$securePass = password_hash($pass,PASSWORD_DEFAULT);
 
     $user = new Usuario();
 
--- extra/respostas-padrao/search.php
+++ extra/respostas-padrao/search.php
@@ -34,7 +34,6 @@
                         </div>
                     </form>
 
-
                     <li class="mx-auto mt-3">
                         <a href="logout.php"><button class="mr-sm-2" type="button">Sair</button></a>
                     </li>
@@ -49,12 +48,14 @@
 
 
         <?php
-
+        require_once "config.php";
         session_start();
-        if ($_SESSION['Usuario']) {
-            require_once "config.php";
 
-            $list = Anotacao::exibeAnotacoes();
+        $search = $_POST['search'];
+
+        if (isset($_POST['btn_pesquisar'])) {
+            $list = Anotacao::search($search);
+
             foreach ($list as $key => $value) {
         ?>
                 <div class="div_txt text-center">
@@ -65,6 +66,8 @@
                         <br>
                         <button type="button" data-toggle="modal" data-target="#modal_editar_resposta<?php echo $key; ?>">Editar</button>
                         <button data-toggle="modal" data-target="#modal_excluir_resposta<?php echo $value['idanotacao'] ?>">Excluir</button>
+                        
+
                     </nav>
                 </div>
 
@@ -74,7 +77,7 @@
                         <div class="modal-content">
                             <form action="editaResposta.php" class="form" method="POST">
                                 <div class="modal-header">
-                                    <input type="text" class="w-100 mr-3 text-center" name="input_titulo" id="input_titulo" value="<?php echo $value['titulo'] ?>">
+                                    <input type="text" name="input_titulo" id="input_titulo" value="<?php echo $value['titulo'] ?>">
                                     <input type="text" name="input_id" id="input_id" hidden value="<?php echo $value['idanotacao'] ?>">
                                     <button type="button" data-dismiss="modal" aria-label="Fechar">
                                         <span aria-hidden="true">&times;</span>
@@ -85,7 +88,7 @@
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" data-dismiss="modal" id="fechar_modal">Fechar</button>
-                                    <button type="submit" name="btn_salvar">Salvar</button>
+                                    <button type="submit" onclick="" name="btn_salvar">Salvar</button>
                                 </div>
                             </form>
                         </div>
@@ -117,47 +120,44 @@
                 </div>
 
 
-
-            <?php
+        <?php
             }
+        }
+        ?>
 
-            ?>
-
-            <!-- Modal modal_cadastrar_resposta-->
-            <div class="modal fade" id="modal_cadastrar_resposta" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <form action="salvaResposta.php" class="form" method="POST">
-                            <div class="modal-header">
-                                <input type="text" name="input_titulo" id="input_titulo" placeholder="Título" class="w-100 mr-3 text-center" required>
-                                <button type="button" data-dismiss="modal" aria-label="Fechar">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <textarea class="minha_textarea" name="textarea_modal" id="textarea_modal" cols="30" rows="10" placeholder="Nova Resposta" required></textarea>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" data-dismiss="modal" id="fechar_modal">Fechar</button>
-                                <button type="submit" onclick="" name="btn_salvar">Salvar</button>
-                            </div>
-                        </form>
-                    </div>
+        <!-- Modal modal_cadastrar_resposta-->
+        <div class="modal fade" id="modal_cadastrar_resposta" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form action="salvaResposta.php" class="form" method="POST">
+                        <div class="modal-header">
+                            <input type="text" name="input_titulo" id="input_titulo" placeholder="Título" required>
+                            <button type="button" data-dismiss="modal" aria-label="Fechar">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <textarea class="minha_textarea" name="textarea_modal" id="textarea_modal" cols="30" rows="10" placeholder="Nova Resposta" required></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" data-dismiss="modal" id="fechar_modal">Fechar</button>
+                            <button type="submit" onclick="" name="btn_salvar">Salvar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
+        </div>
     </section>
-<?php } else {
-            header('Location: login.php');
-        } ?>
 
-<footer>
 
-</footer>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="script.js"></script>
-<script src="script-teste.js"></script>
+    <footer>
+
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+    <script src="script-teste.js"></script>
 </body>
 
 </html>
