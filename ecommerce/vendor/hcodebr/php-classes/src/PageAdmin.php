9cae8f3a7b665f15d4961e4baf3d1c2860bda2cc
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-09-02 16:18:56 -0300

    pageAdmin

---
+++ ecommerce/vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+    
+    public function __construct($opts = array(), $tpl_dir = "/ecommerce/vendor/hcodebr/php-classes/src/views/admin/")
+    {
+        
+    }
+}
