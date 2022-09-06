b3c79165f03923b6f88993c8141e3ea42e69221e
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-09-06 01:06:11 -0300

    inicio crud usuarios

--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,6 +4,7 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use sql as GlobalSql;
 
 class User extends Model {
 
@@ -63,5 +64,12 @@ class User extends Model {
     public static function logout()
     {
         $_SESSION[User::SESSION] = NULL;
+    }
+
+    public static function listAll()
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) ORDER BY b.desperson");
     }
 }
