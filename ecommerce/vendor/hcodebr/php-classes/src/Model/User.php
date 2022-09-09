c68477f34f90414b1234266ac032ff2dfee1db7d
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-09-09 12:55:55 -0300

    reset pass

--- ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/User.php
@@ -177,4 +177,53 @@ class User extends Model
             }
         }
     }
+
+    public static function validForgotDecrypt($code)
+    {
+        define('SECRET', pack('a16', 'senha'));
+        $idrecovery = base64_decode(
+            openssl_decrypt($code,'AES-128-CBC', SECRET, 0, SECRET)
+        );
+
+        $sql = new Sql;
+
+        $result = $sql->select("
+        SELECT * FROM tb_userspasswordsrecoveries a
+        INNER JOIN tb_users b USING (iduser)
+        INNER JOIN tb_persons c USING (idperson)
+        WHERE
+            a.idrecovery = :idrecovery
+            AND
+            a.dtrecovery IS NULL
+            AND
+            DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+        ", array(":idrecovery" => $idrecovery));
+
+        if(!count($result))
+        {
+            throw new \Exception("Não foi possível recuperar a senha");            
+        }
+        else
+        {
+            return $result[0];
+        }
+    }
+
+    public static function setForgotUser($idrecovery)
+    {
+        $sql = new Sql;
+
+
+        $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(":idrecovery" => $idrecovery));
+    }
+
+    public function setPassWord($pass)
+    {
+        $sql = new Sql;
+
+        $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+            ":password" => $pass,
+            ":iduser" => $this->getiduser()
+        ));
+    }
 }
