f72b7e482efb6a0ba3423df7fda332452ede2c94
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-09-12 09:54:07 -0300

    ajuste listagem categorias

--- ecommerce/views-cache/categories-menu.ed2acdef308de694b60c67a9e2076c8f.rtpl.php
+++ ecommerce/views-cache/categories-menu.ed2acdef308de694b60c67a9e2076c8f.rtpl.php
@@ -1,1 +1,7 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>    <li><a href="#">Categoria Um</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/category/5">Acessórios</a></li>
+            <li><a href="/category/7">Capas</a></li>
+            <li><a href="/category/4">Computadores</a></li>
+            <li><a href="/category/9">Material Escolar</a></li>
+            <li><a href="/category/6">Películas</a></li>
+            <li><a href="/category/3">Telefonia</a></li>
+            
