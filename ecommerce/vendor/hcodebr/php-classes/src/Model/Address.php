7b86188fc6503d3be649d934638ad1ff99c77d73
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-09-27 22:30:54 -0300

    carregamento e tratamento tela checkout

--- ecommerce/vendor/hcodebr/php-classes/src/Model/Address.php
+++ ecommerce/vendor/hcodebr/php-classes/src/Model/Address.php
@@ -7,6 +7,8 @@ use \Hcode\Model;
 
 class Address extends Model
 {
+    const SESSION_ERROR = "AddressError";
+
     public static function getCEP($nrZipcode)    
     {
         //https://viacep.com.br/ws/97050700/json/
@@ -40,7 +42,48 @@ class Address extends Model
             $this->setdescity($data['localidade']);
             $this->setdesstate($data['uf']);
             $this->setdescountry('Brasil');
-            $this->setnrzipcode($nrZipcode);
+            $this->setdeszipcode($nrZipcode);
         }
+    }
+
+    public function save()
+    {
+        $sql = new Sql;
+
+        $result = $sql->select('CALL sp_addresses_save (:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)',[
+            'idaddress'=>$this->getidaddress(),
+            'idperson'=>$this->getidperson(),
+            'desaddress'=>$this->getdesaddress(),
+            'descomplement'=>$this->getdescomplement(),
+            'descity'=>$this->getdescity(),
+            'desstate'=>$this->getdesstate(),
+            'descountry'=>$this->getdescountry(),
+            'deszipcode'=>$this->getdeszipcode(),
+            'desdistrict'=>$this->getdesdistrict()
+        ]);
+
+        if(count($result) > 0)
+        {
+            $this->setData($result[0]);
+        }
+
+    }
+
+    
+    public static function setMsgError($msg)//sessao para passar a mensagem de erro no calculo do frete, caso exista
+    {
+        $_SESSION[Address::SESSION_ERROR] = (string)$msg;        
+    }
+
+    public static function getMsgError()
+    {
+        $msg = (isset($_SESSION[Address::SESSION_ERROR])) ? $_SESSION[Address::SESSION_ERROR] : "";
+       
+        return $msg;
+    }
+
+    public static function clearMsgError()
+    {
+        $_SESSION[Address::SESSION_ERROR] = NULL;
     }
 }
