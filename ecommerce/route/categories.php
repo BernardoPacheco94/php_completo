fe650d158480b8cc9aeb02377013776d3ffe8571
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-10-05 17:07:23 -0300

    ajuste categorias

--- ecommerce/route/categories.php
+++ ecommerce/route/categories.php
@@ -15,7 +15,8 @@ $app -> get("/admin/categories", function(){
 	$page = new PageAdmin();
 
 	$page->setTpl("categories", [
-		'categories' => $categories
+		'categories' => $categories,
+		'search' => ''
 	]);
 });
 
